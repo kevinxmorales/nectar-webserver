4f59e2a17b8ad800a0ab7310c43dc65585f03693
Kevin Morales <kevinm2052@gmail.com>
2022-06-18 16:44:59 -0400

    Switched to using pointers for service calls and started using the generic response sending functions

--- internal/transport/http/user.go
+++ internal/transport/http/user.go
@@ -3,9 +3,9 @@ package http
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
 	"gitlab.com/kevinmorales/nectar-rest-api/internal/user"
 	"net/http"
 	"net/mail"
@@ -13,24 +13,26 @@ import (
 )
 
 type UserService interface {
-	GetUser(context.Context, string) (user.User, error)
-	GetUserByEmail(context.Context, string) (user.User, error)
-	AddUser(context.Context, user.User) (user.User, error)
+	GetUser(context.Context, string) (*user.User, error)
+	GetUserByEmail(context.Context, string) (*user.User, error)
+	AddUser(context.Context, user.User) (*user.User, error)
 	DeleteUser(context.Context, string) error
-	UpdateUser(context.Context, string, user.User) (user.User, error)
+	UpdateUser(context.Context, string, user.User) (*user.User, error)
 }
 
 type PostUserRequest struct {
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required"`
+	Password  string `json:"password" validate:"required"`
 }
 
 func convertUserRequestToUser(u PostUserRequest) user.User {
 	return user.User{
-		Name:     u.Name,
-		Email:    u.Email,
-		Password: u.Password,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Email:     u.Email,
+		Password:  u.Password,
 	}
 }
 
@@ -42,112 +44,95 @@ func isValidEmail(email string) bool {
 func (h *Handler) PostUser(w http.ResponseWriter, r *http.Request) {
 	var userRequest PostUserRequest
 	if err := json.NewDecoder(r.Body).Decode(&userRequest); err != nil {
-		log.Error(err)
-		http.Error(w, "unable to decode request", http.StatusInternalServerError)
+		sendBadRequestResponse(w, r, errors.New("unable to decode request"))
 		return
 	}
 	validate := validator.New()
 	err := validate.Struct(userRequest)
 	if err != nil {
-		log.Error(err)
-		http.Error(w, "not a valid user object", http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("not a valid user object"))
 		return
 	}
 	if !isValidEmail(userRequest.Email) {
-		http.Error(w, "not a valid email address format", http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("not a valid email address format"))
 		return
 	}
 	convertedUser := convertUserRequestToUser(userRequest)
 	insertedUser, err := h.UserService.AddUser(r.Context(), convertedUser)
 	if err != nil {
-		log.Error(err)
+		send500Response(w, r, err)
 		return
 	}
-	if err := json.NewEncoder(w).Encode(insertedUser); err != nil {
-		panic(err)
-	}
+	sendOkResponse(w, r, insertedUser)
 }
 
 func (h *Handler) GetUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	if id == EMPTY {
-		w.WriteHeader(http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("no id was supplied with this request"))
 		return
 	}
-	log.Info("in GetUser", id)
 	usr, err := h.UserService.GetUser(r.Context(), id)
 	if err != nil {
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		send500Response(w, r, err)
 		return
 	}
-	log.Info("after service call")
-	if err := json.NewEncoder(w).Encode(usr); err != nil {
-		panic(err)
-	}
+	sendOkResponse(w, r, usr)
 }
 
 func (h *Handler) GetUserByEmail(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	encodedEmail := vars["email"]
 	if encodedEmail == EMPTY {
-		w.WriteHeader(http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("no id was supplied with this request"))
 		return
 	}
 	email, err := url.QueryUnescape(encodedEmail)
 	if !isValidEmail(email) {
-		http.Error(w, "not a valid email address format", http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("not a valid email address format"))
 		return
 	}
 	usr, err := h.UserService.GetUser(r.Context(), email)
 	if err != nil {
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		send500Response(w, r, err)
 		return
 	}
-	if err := json.NewEncoder(w).Encode(usr); err != nil {
-		panic(err)
-	}
+	sendOkResponse(w, r, usr)
 }
 
 func (h *Handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	if id == EMPTY {
-		w.WriteHeader(http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("no id was supplied with this request"))
 		return
 	}
 	var usr user.User
 	if err := json.NewDecoder(r.Body).Decode(&usr); err != nil {
-		log.Error(err)
-		http.Error(w, "unable to decode request", http.StatusInternalServerError)
-	}
-	usr, err := h.UserService.UpdateUser(r.Context(), id, usr)
-	if err != nil {
-		log.Error(err)
-		http.Error(w, "unable to update user", http.StatusBadRequest)
+		send500Response(w, r, err)
 		return
 	}
-	if err := json.NewEncoder(w).Encode(usr); err != nil {
-		panic(err)
+	usrUpdated, err := h.UserService.UpdateUser(r.Context(), id, usr)
+	if err != nil {
+		send500Response(w, r, err)
+		return
 	}
+	sendOkResponse(w, r, usrUpdated)
 }
 
 func (h *Handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	if id == EMPTY {
-		w.WriteHeader(http.StatusBadRequest)
+		sendBadRequestResponse(w, r, errors.New("no id was supplied with this request"))
 		return
 	}
 	err := h.UserService.DeleteUser(r.Context(), id)
 	if err != nil {
-		log.Error(err)
-		http.Error(w, "unable to delete user", http.StatusInternalServerError)
+		send500Response(w, r, err)
 		return
 	}
-	if err = json.NewEncoder(w).Encode(Response{Message: "successfully deleted user"}); err != nil {
-		panic(err)
-	}
+	res := Response{Message: "successfully deleted user"}
+	sendOkResponse(w, r, res)
 }
