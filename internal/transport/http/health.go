cbb3c077b0e452b9c10c42ddb715458b8a397b88
Kevin Morales <kevinm2052@gmail.com>
2023-02-01 21:11:18 -0500

    added more logging to health check

--- internal/transport/http/health.go
+++ internal/transport/http/health.go
@@ -13,13 +13,15 @@ type HealthService interface {
 
 func (h *Handler) healthCheck(w http.ResponseWriter, r *http.Request) {
 	if err := h.HealthService.CheckDbHealth(r.Context()); err != nil {
-		log.Info(fmt.Sprintf("unsuccessful request, status code: %d", http.StatusInternalServerError))
+		log.Info(fmt.Sprintf("unsuccessful request, status code: %d, reason: %v", http.StatusInternalServerError, err))
 		w.WriteHeader(http.StatusInternalServerError)
 		h.encodeJsonResponse(&w, Response{Message: "Service Unhealthy"})
 		return
 	}
-	log.Info(fmt.Sprintf("successfully handled request, status code: %d", http.StatusOK))
 	w.WriteHeader(http.StatusOK)
+	message := "Service alive. Database connection is good."
+	log.Info(message)
+	log.Info(fmt.Sprintf("successfully handled request, status code: %d", http.StatusOK))
 	h.encodeJsonResponse(&w, Response{Message: "Service alive. Database connection is good."})
 	return
 }
