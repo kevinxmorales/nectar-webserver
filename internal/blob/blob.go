fc16eb945565de38bfd99a659c6c481a386a443e
Kevin Morales <kevinm2052@gmail.com>
2023-02-19 20:32:37 -0500

    refactor blob service functions

--- internal/blob/blob.go
+++ internal/blob/blob.go
@@ -20,6 +20,8 @@ type S3Response struct {
 }
 
 type Service struct {
+	Bucket      string
+	Acl         string
 	BlobSession *session.Session
 }
 
@@ -33,71 +35,65 @@ func NewService() (*Service, error) {
 		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
 	})
 	if err != nil {
-		log.Info("FAILED to create S3 session: ", err.Error())
+		log.Errorf("FAILED to create S3 session: %s", err.Error())
 		return nil, err
 	}
 	log.Info("successfully created S3 session")
 
-	return &Service{BlobSession: sess}, nil
+	s := &Service{
+		BlobSession: sess,
+		Bucket:      os.Getenv("S3_BUCKET"),
+		Acl:         os.Getenv("AWS_ACL"),
+	}
+	return s, nil
 }
 
 func (s *Service) UploadToBlobStore(fileList []string, ctx context.Context) ([]string, error) {
-	//Set up the S3 bucket
-	bucket := os.Getenv("S3_BUCKET")
-	acl := os.Getenv("AWS_ACL")
 
 	//Set up the concurrency
-	//Need wait group and mutex
 	var wg sync.WaitGroup
 
 	// The resulting s3 urls to the files
 	s3Urls := make([]string, len(fileList))
 	s3Errors := make([]error, len(fileList))
 
+	handleClose := func(file *os.File, index int) {
+		if err := file.Close(); err != nil {
+			log.Error(err)
+			s3Errors[index] = err
+		}
+	}
+	uploadImage := func(pathOfFile string, index int, wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		file, err := os.Open(pathOfFile)
+		if err != nil {
+			s3Errors[index] = err
+			return
+		}
+		defer handleClose(file, index)
+
+		uploader := s3manager.NewUploader(s.BlobSession)
+		input := &s3manager.UploadInput{
+			ACL:    aws.String(s.Acl),
+			Bucket: aws.String(s.Bucket),
+			Key:    aws.String(file.Name()),
+			Body:   file,
+		}
+		result, err := uploader.UploadWithContext(ctx, input)
+		if err != nil {
+			s3Errors[index] = err
+			return
+		}
+		s3Urls[index] = result.Location
+		log.Info(fmt.Sprintf("Upload result: %+v\n", result))
+	}
+
 	// Iterate over the local files that need to be updated
 	for i, pathOfFile := range fileList {
-		handleClose := func(file *os.File) {
-			if err := file.Close(); err != nil {
-				log.Error(err)
-				s3Errors[i] = err
-			}
-		}
-		logAndStoreError := func(err error) {
-			log.Error(err)
-			s3Errors[i] = err
-		}
-
 		// Kick off goroutine with thread-safe function to upload to s3
 		wg.Add(1)
-		i := i
-		go func(pathOfFile string, wg *sync.WaitGroup) {
-			defer func() {
-				wg.Done()
-			}()
-			file, err := os.Open(pathOfFile)
-			if err != nil {
-				logAndStoreError(err)
-				return
-			}
-			defer func() {
-				handleClose(file)
-			}()
-			uploader := s3manager.NewUploader(s.BlobSession)
-			result, err := uploader.UploadWithContext(
-				ctx,
-				&s3manager.UploadInput{
-					ACL:    aws.String(acl),
-					Bucket: aws.String(bucket),
-					Key:    aws.String(file.Name()),
-					Body:   file,
-				})
-			if err != nil {
-				logAndStoreError(err)
-				return
-			}
-			s3Urls[i] = result.Location
-			log.Info(fmt.Sprintf("Upload result: %+v\n", result))
-		}(pathOfFile, &wg)
+		go uploadImage(pathOfFile, i, &wg)
 	}
 	// Block until WaitGroup counter is zero, then return the s3 urls
 	wg.Wait()
@@ -105,32 +101,25 @@ func (s *Service) UploadToBlobStore(fileList []string, ctx context.Context) ([]s
 	//Check if any errors occurred
 	for _, err := range s3Errors {
 		if err != nil {
+			log.Error(err)
 			return nil, err
 		}
 	}
-
 	return s3Urls, nil
 }
 
 func (s *Service) DeleteFromBlobStore(fileName string) error {
 	service := s3.New(s.BlobSession)
-	bucket := os.Getenv("S3_BUCKET")
 	input := &s3.DeleteObjectInput{
-		Bucket: aws.String(bucket),
+		Bucket: aws.String(s.Bucket),
 		Key:    aws.String(fileName),
 	}
 	result, err := service.DeleteObject(input)
 	if err != nil {
 		if awsError, ok := err.(awserr.Error); ok {
-			switch awsError.Code() {
-			default:
-				log.Error(awsError.Error())
-				return awsError
-			}
-		} else {
-			log.Error(err.Error())
-			return err
+			log.Errorf("error occurred on AWS side of transaction: %v", awsError)
 		}
+		return err
 	}
 	log.Info("Successfully deleted item from S3", result.String())
 	return nil
